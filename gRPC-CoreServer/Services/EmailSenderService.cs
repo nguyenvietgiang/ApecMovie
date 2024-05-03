ae8137d6f1f7007a3575f767fb86b869b6af2640
Giang <nguyenvietgiang1110@gmail.com>
2024-05-03 13:10:09 +0700

    Dựng core cho GRPC

---
+++ gRPC-CoreServer/Services/EmailSenderService.cs
@@ -0,0 +1,16 @@
+﻿using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
+using GrpcEmailService;
+
+
+namespace gRPC_CoreServer.Services
+{
+    public class EmailSenderService: GrpcEmailService.EmailSender.EmailSenderBase
+    {
+        public override Task<EmailResponse> SendEmail(EmailRequest request, ServerCallContext context)
+        {
+            string message = $"Email sent to {request.To} with subject '{request.Subject}' and body '{request.Body}'";
+            return Task.FromResult(new EmailResponse { Message = message });
+        }
+    }
+}
