5feabb239588970ecb37abf9fcbcfaa7492309a3
Giang <nguyenvietgiang1110@gmail.com>
2024-05-08 17:14:56 +0700

    Setup paypal services

--- PaymentServices.Api/Program.cs
+++ PaymentServices.Api/Program.cs
@@ -1,3 +1,5 @@
+using PaymentServices.Api.Client;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// paypal client configuration
+builder.Services.AddSingleton(x =>
+    new PaypalClient(
+        builder.Configuration["PayPalOptions:ClientId"],
+        builder.Configuration["PayPalOptions:ClientSecret"],
+        builder.Configuration["PaypalOptions:Mode"]
+    )
+);
 
 var app = builder.Build();
 
