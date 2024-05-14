200a5dd05b45ae8b2b1785eb79d062d90108ee90
Giang <nguyenvietgiang1110@gmail.com>
2024-05-14 11:44:04 +0700

    Dựng layout ban đầu cho portal

--- ApecMoviePortal/Controllers/AuthController.cs
+++ ApecMoviePortal/Controllers/AuthController.cs
@@ -8,5 +8,10 @@ namespace ApecMoviePortal.Controllers
         {
             return View();
         }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
     }
 }
