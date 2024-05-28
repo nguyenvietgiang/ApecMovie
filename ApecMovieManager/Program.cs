23d34e20c16444940759a40dc1e623976e79d7a0
Giang <nguyenvietgiang1110@gmail.com>
2024-05-28 17:31:31 +0700

    Chuyển sang dùng thư viện UI khác

--- ApecMovieManager/Program.cs
+++ ApecMovieManager/Program.cs
@@ -6,8 +6,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7128/v1/api") });
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddIgniteUIBlazor();
 
 
 await builder.Build().RunAsync();
