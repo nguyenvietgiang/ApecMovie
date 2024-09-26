43b0f39e05f1a1e53fd8d6d0ba36e8ac7142bc20
Giang <nguyenvietgiang1110@gmail.com>
2024-09-26 21:13:40 +0700

    quản lý và xây dựng hệ thống với Dotnet Aspire Hosting.

---
+++ ApecMovieAppHost/Program.cs
@@ -0,0 +1,8 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var movieApi = builder.AddProject<Projects.MovieServices_Api>("MovieServices-Api");
+var userApi = builder.AddProject<Projects.UserServices_Api>("UserServices-Api");
+var ticketApi = builder.AddProject<Projects.TicketServices_Api>("TicketServices-Api");
+var emailApi = builder.AddProject<Projects.EmailServices_Api>("EmailServices-Api");
+
+builder.Build().Run();
