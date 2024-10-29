f6771293e0ee17aeab0430a0aa9674422ac50548
Giang <nguyenvietgiang1110@gmail.com>
2024-10-29 22:20:43 +0700

    Host đang không chạy được nhiều port với Movie API

--- ApecMovieAppHost/Program.cs
+++ ApecMovieAppHost/Program.cs
@@ -1,9 +1,21 @@
-var builder = DistributedApplication.CreateBuilder(args);
+﻿var builder = DistributedApplication.CreateBuilder(args);
+// api
+// đối với Movie API nên chạy riêng vì đang cần chạy nhiều port để load balancer
+//var movieApi = builder.AddProject<Projects.MovieServices_Api>("MovieServices-Api");
 
-var movieApi = builder.AddProject<Projects.MovieServices_Api>("MovieServices-Api");
 var userApi = builder.AddProject<Projects.UserServices_Api>("UserServices-Api");
 var ticketApi = builder.AddProject<Projects.TicketServices_Api>("TicketServices-Api");
 var emailApi = builder.AddProject<Projects.EmailServices_Api>("EmailServices-Api");
 var blogApi = builder.AddProject<Projects.BlogServices>("BlogServices-Api");
+var logApi = builder.AddProject<Projects.LogServices_API>("LogServices-Api");
+var aggregatorApi = builder.AddProject<Projects.AggregatorService_API>("AggregatorService-Api");
+
+
+// gateway
+var gateway = builder.AddProject<Projects.ApecMovieGateWay>("Gateway");
+
+// fe
+builder.AddProject<Projects.ApecMoviePortal>("ApecMoviePortal");
+builder.AddProject<Projects.ApecMovieManager>("ApecMovieManager");
 
 builder.Build().Run();
